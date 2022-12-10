5f0a6f7e548b2efe88441eb3b9c2eec3c242051f
MostafaHQ <mostafa.hajqasem@gmail.com>
2022-12-10 07:48:29 +0200

    Putting it Toghether Assignment

--- React/Prop It Up/prop-it-up/src/components/personCard.jsx
+++ React/Prop It Up/prop-it-up/src/components/personCard.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {birthday : this.props.age};
+    }
+    
+    getOlder = () => this.setState({birthday: this.state.birthday +1});
+
 
     render() { 
         return (
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.birthday}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.getOlder}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         );
     }
